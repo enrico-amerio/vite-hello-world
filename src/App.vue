0ae897312c65723996e9322e170eed546216c66b
enrico-amerio <amerio.enrico95@gmail.com>
2024-03-20 14:15:00 +0100

    [add] Esercizio finito

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  data(){
+    return{
+      title: 'Onion Rings',
+      paragraph: 'One ring to rule them all, one ring to find them, One ring to bring them all and in the darkness bind them.',
+    }
+  }
+}
 </script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <img src="./assets/food-onion-rings.svg" alt="onion" class="onion">
+    <h1>{{ title }}</h1>
+    <p>{{ paragraph }}</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .onion{
+    width:350px;
+  }
 </style>
